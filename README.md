1abd8e9a8fee57b93bd9adae9ea0f5ea2602f39d
tkrsh <utkarsh.srivastava123@outlook.com>
2020-04-27 23:17:39 +0530

    modified:   README.md 	modified:   covid_updates.py

---
+++ README.md
@@ -0,0 +1,30 @@
+# Get informed about new COVID-19 cases confirmed in your country 
+
+<p align = "center">
+<img src="https://imgur.com/a/nAIkXxb">
+ </p>
+
+## What does this script do ?
+
+    1) Displays live count of COVID-19 cases in your Country.
+    2) Displays number of New Cases wrt previus update
+    3) Plays Sound Notification when new Cases are Detected     ** Disabled By Default  To Enable : Uncomment Line 33 
+    4) Sends A Slack Message Alert                              ** Disabled By Default  
+
+## Procedure 
+
+    1) Download the script 
+    2) Run The Script in terminal
+        - python3 covid_updates.py CountryName SoundPath
+    3) To run script in background with slack and music notifications , run in terminal 
+        - nohup sh updates.sh &&
+ 
+## Installing Dependencies 
+    
+    -pip install playsound
+
+### ** To Enable Slack Notification 
+
+    - Get the Slack Access token url and modify the script with your access token url and uncomment following lines [12,27,28,29]
+
+        ex- https://hooks.slack.com/services/xxxxxxxxx
--- covid_updates.py
+++ covid_updates.py
@@ -8,11 +8,13 @@ import sys
 import json
 z=sys.argv[1]
 url= "https://google.com/covid19-map/?hl=en"
+#slack_url=""
 r= requests.get(url)
 df= pd.read_html(r.text)[0]
 num=int(df.loc[df['Location']==z,"Confirmed"])
 print("Current Cases: ",num)
 i=0
+
 while True:
     i+=1
     r= requests.get(url)
@@ -22,21 +24,27 @@ while True:
         now=datetime.now()
         new_cases=num_new-num
         timez= now.strftime("%m-%d %H:%M:%S")
+        #Message= f"Updated Number Of Cases In {Z}: {num_new}\nNumber of New Cases: {new_cases}\nUpdate at: {timez}"
+        #slack_msg= {'text':Message}
+        #requests.post(web,data=json.dumps(slack_msg))
         print("----------------------")
-        print("Previous Cases",num)
+        print("Previous Cases:",num)
         print("Current Cases: ",num_new)
-        print("New Cases Found : \n",num_new-num)
-        playsound('alarm.mp3')
+        print("New Cases Found : \n",num_new-num," At:",timez)
+        #playsound("Path_To_Sound")
         num=num_new
         del(df)
         del(r)
     else:
+        now=datetime.now()
+        timez= now.strftime("%H:%M:%S")
         print("----------------------")
-        print("Previous Cases",num)
+        print("Fetching Information for",z)
+        print("Previous Cases:",num)
         print("Current Cases: ",num_new)
         print("New Cases:",num_new-num)
-        print("Number of times data refreshed:",i)
+        print("Number of times data refreshed:",i,"\nLast Refresh At:",timez)
         del(df)
         del(r)
-        time.sleep(300)
+        time.sleep(5)
         
